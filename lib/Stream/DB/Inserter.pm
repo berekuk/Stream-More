158fadb799f9673a02bf32e7e8b642ad411d6048
Vyacheslav Matjukhin <mmcleric@yandex-team.ru>
2009-09-08 05:07:31 +0400

    * two experimental modules:  - Stream::DB::Inserter  - Stream::Bin::Filter

---
+++ lib/Stream/DB/Inserter.pm
@@ -0,0 +1,55 @@
+package Stream::DB::Inserter;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Stream::DB::Inserter - simple stream wrapper around PPB::DB::Inserter
+
+=head1 SYNOPSIS
+
+    use Stream::DB::Inserter;
+
+    $inserter = new Stream::DB::Inserter({
+        DB => connectdb('foaf'),
+        Insert => 'INSERT agents(link, foaf_link)',
+        Values => '?,?',
+        Tail => 'ON DUPLICATE KEY UPDATE link = VALUES(link)', # optional
+        PortionSize => 1000,    # optional
+    });
+    $inserter->write(['http://a', 'http://a/foaf']);
+    ...
+    $inserter->commit();
+
+=cut
+
+use Yandex::Version '{{DEBIAN_VERSION}}';
+
+use base qw(Stream::Out);
+use PPB::DB::Inserter;
+
+sub new {
+    my $class = shift;
+    my $inserter = PPB::DB::Inserter->new(@_);
+    return bless { inserter => $inserter } => $class;
+}
+
+sub write {
+    my ($self, $row) = @_;
+    $self->{inserter}->insert(@$row);
+}
+
+sub commit {
+    my $self = shift;
+    $self->{inserter}->finish;
+}
+
+=head1 AUTHOR
+
+Vyacheslav Matjukhin <mmcleric@yandex-team.ru>
+
+=cut
+
+1;
+
