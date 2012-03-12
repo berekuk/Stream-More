0301872af43cd1308dd4b3a32f57d5d733ca5621
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-03-12 23:40:24 +0400

    some POD for basic roles

--- lib/Stream/Moose/In.pm
+++ lib/Stream/Moose/In.pm
@@ -2,9 +2,21 @@ package Stream::Moose::In;
 
 # ABSTRACT: role for stream storage classes
 
+=head1 DESCRIPTION
+
+Moose-based input streams should implement this role.
+
+=cut
+
 use Moose::Role;
 with 'Stream::Moose::FakeIsa' => { extra => ['Stream::In'] };
 
 requires 'read', 'read_chunk', 'commit';
+
+=head1 SEE ALSO
+
+L<Stream::In> - base API description lives there by now.
+
+=cut
 
 1;
